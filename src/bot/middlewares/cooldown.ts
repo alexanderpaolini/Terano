dafc01a0c95de0c2cbfbb0a6c38d3abd62872031
Million900o <apaolini900o@gmail.com>
2021-02-27 01:11:37 -0500

    features v2

--- src/bot/middlewares/cooldown.ts
+++ src/bot/middlewares/cooldown.ts
@@ -1,9 +1,6 @@
 import { CommandContext } from "discord-rose/dist/typings/lib";
 
 export default async (ctx: any) => {
-  if (ctx.command.cooldown) {
-    // const isOwner = !!await ctx.worker.db.userDB.getOwner(ctx.message.author.id);
-    // if (isOwner) return true;
-    return true;
-  }
+  // TODO: Work on this.
+  return true;
 };
