43f304dee74f0ace21bd1b53f5c2b782b31e229b
Million900o <apaolini900o@gmail.com>
2021-05-14 14:29:42 +0000

    oops

--- src/bot/structures/CommandContext.ts
+++ src/bot/structures/CommandContext.ts
@@ -64,10 +64,10 @@ export default class CMDCTX extends CommandContext {
     if (this.flags.noreply) options.reply = false
     if (this.flags.nomention) options.mention = false
 
-    const cmd = `| ${String(await this.lang(`CMD_${this.command.locale}_NAME` as LanguageString))}`
+    const cmd = ` | ${String(await this.lang(`CMD_${this.command.locale}_NAME` as LanguageString))}`
 
     const response = await this.embed
-      .author(this.message.member?.nick ? this.message.member.nick + cmd : `${this.message.author.username} ` + cmd, getAvatar(this.message.author))
+      .author(this.message.member?.nick ? this.message.member.nick + cmd : `${this.message.author.username}` + cmd, getAvatar(this.message.author))
       .description(message)
       .color(options.color ?? (options.error ? this.worker.colors.RED : this.worker.colors.GREEN))
       .send(options.reply, !!options.mention)
