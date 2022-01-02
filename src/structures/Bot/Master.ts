4dc6da1decdc891a06dae47473b163715becc000
Million900o <apaolini900o@gmail.com>
2022-01-02 01:31:07 -0500

    feat(logging): implement logging with winston

--- src/structures/Bot/Master.ts
+++ src/structures/Bot/Master.ts
@@ -2,10 +2,26 @@ import * as Jadl from 'jadl'
 
 import path from 'path'
 
+import { createLogger, transports, format } from 'winston'
+
 import { Config } from '../Config'
 
 export class Master extends Jadl.Master {
   config = Config
+
+  logger = createLogger({
+    level: 'debug',
+    format: format.combine(
+      format.colorize({ colors: { info: 'blue', debug: 'magenta', warn: 'yellow', error: 'red', silly: 'rainbow' }, level: true }),
+      format.label({ label: 'BOT' }),
+      format.errors({ stack: true }),
+      format.splat(),
+      format.printf((info) => `[${info.label as string}] [${info.level}]: ${info.message}`)
+    ),
+    transports: [
+      new transports.Console()
+    ]
+  })
 
   constructor () {
     super(
@@ -22,9 +38,11 @@ export class Master extends Jadl.Master {
       }
     )
 
+    this.log = (msg: string, c?: Jadl.Cluster) => this.logger.info(c ? `Cluster ${c.id} | ${msg}` : msg)
+
     this
       .on('DEBUG', (msg) => {
-        if (process.env.NODE_ENV === 'development') console.log(msg)
+        if (process.env.NODE_ENV === 'development') this.logger.debug(msg)
       })
   }
 }
