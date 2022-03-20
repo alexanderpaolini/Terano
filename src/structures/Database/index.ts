a7bcf6ac77e930c4986a91e300db90498b7c5e5d
Million900o <apaolini900o@gmail.com>
2022-03-20 13:23:56 -0400

    refactor: remove unused API code
    Removed old, unused API for votes and such

--- src/structures/Database/index.ts
+++ src/structures/Database/index.ts
@@ -1,15 +1,11 @@
 import mongoose from 'mongoose'
-
 import { Config } from '../Config'
-
 import { GuildDb } from './GuildDb'
 import { UserDb } from './UserDb'
-import { VoteDb } from './VoteDb'
 
 export class Database {
   guilds = new GuildDb()
   users = new UserDb()
-  votes = new VoteDb()
 
   constructor () {
     mongoose.connect(Config.db.connection_string, {
@@ -22,4 +18,4 @@ export class Database {
   }
 }
 
-export { GuildDb, UserDb, VoteDb }
+export { GuildDb, UserDb }
